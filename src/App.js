5a741b105e5219916d430fc678c2af6d6c75d8ff
javedwbst <javedwbst@gmail.com>
2024-08-03 10:06:27 +0530

    issue resolved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/header";
 import { Route, Routes, useLocation } from "react-router-dom";
@@ -10,12 +9,12 @@ function App() {
 
   return (
     <>
-      <div className="container mx-auto min-h-screen flex">
-        <div className={`gap-4 w-full ${location.pathname == "/projects" ? "grid-cols-1" : "grid grid-cols-2"}`}>
-          <div className="flex flex-col py-20">
+      <div className="container mx-auto md:px-14 px-5 min-h-screen flex">
+        <div className={`gap-4 w-full ${location.pathname === "/projects" ? "grid-cols-1" : "grid lg:grid-cols-2"}`}>
+          <div className="flex flex-col lg:py-20">
             <Header />
           </div>
-          <div className={`gap-4 w-full ${location.pathname == "/projects" ? "pb-20 px-10" : "pb-20 ps-20"}`}>
+          <div className={`gap-4 w-full ${location.pathname === "/projects" ? "pb-20 px-10" : "pb-20 lg:ps-20"}`}>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/projects" element={<Projects />} />
--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -4,23 +4,15 @@
 
 const AboutMe = () => {
   return (
-    <div className='text-lg'>
+    <div className='text-lg'> 
        <div className=''>
-       <h2 className="mb-3 text-xl font-semibold sticky pt-20 top-0 z-10 bg-slate-50">About Me.</h2>
+       <h2 className="mb-3 text-xl font-semibold md:sticky md:pt-20 top-0 z-10 bg-slate-50">About Me.</h2>
         <p className='text-slate-600 text-[16px] font-normal leading-normal mb-5'>Develop, style, and deliver high-quality websites, design systems, mobile applications, and cross-platform digital experiences for a variety of clients using technologies such as JavaScript, React, Tailwind CSS, Bootstrap, Shopify, WordPress, Contentful, and more.</p>
         <p className='text-slate-600 text-[16px] font-normal leading-normal mb-5'>My design philosophy revolves around user-centered design. I believe that a great design is not just about aesthetics but also about how it works for the end-user. My goal is to create designs that are intuitive, engaging, and accessible to everyone.</p>
         <p className='text-slate-600 text-[16px] font-normal leading-normal' >Ensuring designs look great on all devices, from desktops to mobile phones.</p>
         
        
     </div>
-    {/* <div className="flex mt-5">
-          <Link
-            className="uppercase text-sm font-medium flex items-center gap-2  rounded-md"
-            to='/' target="_blank"
-          >
-            View More<Uparrow />
-          </Link>
-    </div> */}
     </div>
     
   )
